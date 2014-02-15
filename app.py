d6fd96cd9f5424b0ec44533b2665916c8208830e
Rishabh Sagar <Rishabh.Sagar@gmail.com>
2014-02-15 15:59:28 +0000

    Changing hi to hello

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     print "Printing hello world!"
-    return 'Hi World!'
+    return 'Hello World!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, else default to 5000.
