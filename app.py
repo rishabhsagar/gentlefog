5af3ae42cdedebe6d1133f0d5945658f361a766d
Rishabh Sagar <Rishabh.Sagar@gmail.com>
2014-02-16 02:21:00 +0000

    New Functionality --------------------- * url_root/<topic_name> route defined * template functionality added (new directory) * error page created / defined * explore link added to each topic

--- app.py
+++ app.py
@@ -1,13 +1,55 @@
 import os
+import requests
+import xml.etree.ElementTree as ET
 
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
+app.config.update(dict(
+    DEBUG=True
+    ))
 
 @app.route('/')
 def hello():
-    print "Printing hello world!"
-    return 'Hello World!'
+    raw_data = requests.get('http://en.wikipedia.org/w/api.php?action=parse&format=xml&page=Statistics&prop=links|wikitext&contentmodel=wikitext')
+    ascii_data = raw_data.text.encode('ascii', 'ignore')
+    xdoc = ET.fromstring(ascii_data)
+    list_of_links = []
+
+    for link in xdoc.iter('pl'):
+        list_of_links.append(link.text)
+
+    print "List of links has ", len(list_of_links)
+    root = request.url_root
+    return render_template('show_links.html', root=root, title="Statistics", list_of_links=list_of_links)
+
+@app.route('/<topic>')
+def show_topic(topic=None):
+    if request.method == 'GET':
+        payload = {'action':'parse', 'format':'xml', 'page':'sex', 'prop':'links'}
+        payload['page']=topic
+        wikilink = 'http://en.wikipedia.org/w/api.php'
+        
+        raw_data = requests.get(wikilink, params=payload).text
+        ascii_data = raw_data.encode('ascii', 'ignore')
+        xdoc = ET.fromstring(ascii_data)
+    
+        if ( xdoc.find('error') is not None):
+            error = {}
+            error['code'] = xdoc.find('error').attrib['code']
+            error['summary'] = xdoc.find('error').attrib['info']
+            return render_template('show_error.html', error=error)
+        else:
+            list_of_links = []
+            root = request.url_root
+            for link in xdoc.iter('pl'):
+                list_of_links.append(link.text)
+            list_of_links = filter(lambda link: ':' not in link, list_of_links)
+            return render_template('show_links.html', root=root, title=topic, list_of_links=list_of_links)
+    error = {}
+    error['code'] = 'Invalid request type'
+    error['summary'] = 'This page expects only GET type requests'
+    return render_template('show_error.html', error=error)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, else default to 5000.
